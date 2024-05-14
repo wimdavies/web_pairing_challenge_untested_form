5ab96ead930a3e0f6fc6fc53ce5a3a265887b882
wimdavies <william.johnston.davies@gmail.com>
2024-05-14 12:21:38 +0100

    Write test for form happy path. Add user list assertion to test_example. Update conftest.py port

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from app import app
 def test_web_address(xprocess):
     python_executable = sys.executable
     app_file = py.path.local(__file__).dirpath("../app.py")
-    port = "5000"
+    port = "5001"
     class Starter(ProcessStarter):
         env = {"PORT": port, "APP_ENV": "test", **os.environ}
         pattern = "Debugger PIN"
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,19 @@ def test_example(page, test_web_address):
 
     # We assert that it has the the correct content
     expect(heading).to_have_text("User Management")
+
+    # Assert that the user list includes the first user
+    expect(page.locator("ul")).to_contain_text("john")
+
+"""
+Create User form adds a user to the list
+"""
+def test_form_happy_path(page, test_web_address):
+    # We load a virtual browser and navigate to the /books page
+    page.goto(f"http://{test_web_address}")
+
+    page.get_by_label("Username").fill("Bob")
+    page.get_by_role("button").click()
+
+    # Assert that the user list now includes "Bob"
+    expect(page.locator("ul")).to_contain_text("Bob")
